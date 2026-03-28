fb43efd466ce641828183d597cb4dbdb8fcc0a69
pussnboot <132279495+pussnboot@users.noreply.github.com>
2026-03-28 15:53:45 -0400

    Created the EditUserController.java

---
+++ TeamManagementUI/src/main/java/com/example/assigment2/EditUserController.java
@@ -0,0 +1,49 @@
+package com.example.assigment2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class EditUserController {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField dobField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField addressField;
+
+    private Person person;
+
+    public void setPerson(Person p){
+
+        person = p;
+
+        nameField.setText(p.getName());
+        dobField.setText(p.getDob());
+        emailField.setText(p.getEmail());
+        phoneField.setText(p.getPhone());
+        addressField.setText(p.getAddress());
+    }
+
+    @FXML
+    private void handleSave(){
+
+        person.setName(nameField.getText());
+        person.setDob(dobField.getText());
+        person.setEmail(emailField.getText());
+        person.setPhone(phoneField.getText());
+        person.setAddress(addressField.getText());
+
+        Stage stage = (Stage) nameField.getScene().getWindow();
+        stage.close();
+    }
+}
